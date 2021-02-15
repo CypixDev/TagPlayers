8b11b26a970362f8179f5219bd2256916e8c0ad2
Cypix <pi.konermann@gmail.com>
2021-02-15 21:47:27 +0100

    First commit!

--- src/main/java/de/cypix/tagplayers/listeners/ChatListener.java
+++ src/main/java/de/cypix/tagplayers/listeners/ChatListener.java
@@ -1,6 +1,10 @@
 package de.cypix.tagplayers.listeners;
 
+import de.dytanic.cloudnet.CloudNet;
+import de.dytanic.cloudnet.driver.CloudNetDriver;
+import de.dytanic.cloudnet.driver.permission.IPermissionGroup;
 import de.dytanic.cloudnet.driver.permission.IPermissionUser;
+import de.dytanic.cloudnet.driver.permission.PermissionUserGroupInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -9,26 +13,45 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
+import java.util.Objects;
+
 public class ChatListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChat(AsyncPlayerChatEvent e){
-        if(e.getPlayer().hasPermission("TagPlayers")){
-            if(e.getMessage().contains("@")){
-                String playerName = e.getMessage().substring(e.getMessage().indexOf("@") + 1).split(" ")[0];
-                Player tagged = Bukkit.getPlayer(playerName);
-                if(tagged != null && tagged.isOnline()){
-                    tagged.playSound(tagged.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10F, 0.2F);
-                    e.setMessage(e.getMessage().replace("@"+playerName,"§c@"+playerName+"§7"));
+    public void onChat(AsyncPlayerChatEvent e) {
+        System.out.println(e.getMessage());
+        if (e.getPlayer().hasPermission("TagPlayers")) {
+            if (e.getMessage().contains("@")) {
+                String taggedName = e.getMessage().substring(e.getMessage().indexOf("@") + 1).split(" ")[0];
+                System.out.println(taggedName);
+                Player tagged = Bukkit.getPlayer(taggedName);
+                if (tagged != null && tagged.isOnline()) {
+                    tagged.playSound(tagged.getLocation(), Sound.ORB_PICKUP, 10F, 0.2F);
+                    e.setMessage(e.getMessage().replace("@" + taggedName, getGroupColor(tagged) + "@" + taggedName + "§r"));
+                } else {
+                    for (IPermissionGroup group : CloudNetDriver.getInstance().getPermissionManagement().getGroups()) {
+                        if (group.getName().equalsIgnoreCase(taggedName)) {
+                            e.setMessage(e.getMessage().replace("@" + taggedName, group.getColor() + "@" + taggedName + "§r"));
+                            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                                if (Objects.requireNonNull(CloudNetDriver.getInstance().getPermissionManagement().getUser(onlinePlayer.getUniqueId())).inGroup(group.getName())) {
+                                    onlinePlayer.playSound(onlinePlayer.getLocation(), Sound.ORB_PICKUP, 10F, 0.2F);
+                                }
+                            }
+
+                            break;
+                        }
+                    }
                 }
             }
         }
     }
 
 
-    private String getGroupColor(Player player){
-        IPermissionUser permissionUser = CloudPermissionManagement.getInstance().getUser(player.getUniqueId());
-
+    private String getGroupColor(Player player) {
+        //CloudNetDriver.getInstance().getPermissionManagement().get
+        IPermissionUser permissionUser = CloudNetDriver.getInstance().getPermissionManagement().getUser(player.getUniqueId());
+        assert permissionUser != null;
+        return CloudNetDriver.getInstance().getPermissionManagement().getHighestPermissionGroup(permissionUser).getColor();
     }
 
 }
